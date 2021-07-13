e3129407bdcac203117bab0637ad3681464e5aaa
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-07-13 23:01:50 +0200

    fixed problems with multiple models

--- main.py
+++ main.py
@@ -21,12 +21,16 @@ class MakeTests():
             model: self.assertEqual(int(X1, 2), X2)
             cases:
                 - "1" ==> 1
-                - "10" ==> 2
+                - "110" ==> 2
+
+            model: self.assertIsInstance(int(X1, 2), X2)
+            cases:
+                - "1" ==> int
+                - "10" ==> int
         */
         """
         self.file = file
         classes = self.get_docstrings()
-        # classes = MakeTests.filter_tests(classes)
         self.output = ""
 
         for class_ in classes:
@@ -36,55 +40,58 @@ class MakeTests():
         open("test.py", "w").write(self.output)
 
     def docstring_to_test(docstring: str, function_name: str) -> tuple:
-        model = MakeTests.get_model(docstring)
-        cases = re.search("(?<=cases:(\r|\n))((\t|\s)*- .*(\n|\r))*", docstring)
-        if cases is None:
+        models = MakeTests.get_models(docstring)
+        docstring = r"""
+
+            model: self.assertEqual(int(X1, 2), X2)
+            cases:
+                - "1" ==> 1
+                - "110" ==> 2
+
+            model: self.assertIsInstance(int(X1, 2), X2)
+            cases:
+                - "1" ==> int
+                - "10" ==> int
+                    """
+        all_cases = re.findall(r"(?<=cases:\n)((\s*- .*\n)*)", docstring, re.MULTILINE)
+        if all_cases is None:
             raise ValueError(f"Testcases must be pressent after each model. Missing in docstring of function {function_name}.")
             sys.exit()
-        cases = cases.group(0).split("\n")
-        print(cases)
-        cases = [case[case.find("- ") + len("- "):] for case in cases if len(case) > 0]
-        cases = [case.split("==>") for case in cases]
-        return model, cases
+        idx = 0
+        final_cases = []
+        for cases in all_cases:
+            cases = cases[0]
+            print(cases)
+            cases = cases.split("\n")
+            cases = [case for case in cases if len(case) > 0 and case != "\n"]
+            cases = [case[case.find("- ") + len("- "):] for case in cases if len(case) > 0]
+            cases = [case.split("==>") for case in cases]
+            cases = [case for case in cases if len(case) > 0 and case != "\n"]
+            if len(cases) > 0:
+                final_cases.append(cases)
+        return models, final_cases
 
     def write_class(self, classname):
         self.output += open("class_model.py").read().replace("NAME", classname)
 
     def write_method(self, method):
-        name, (model, cases) = method
-        self.output += f"\n\tdef test_{name}(self):"
-        print(model)
-        for case in cases:
-            case_string = f"\n\t\t{model}"
-            for i in range(len(case)):
-                case_string = case_string.replace(f"X{i + 1}", case[i])
-            print(case_string, type(case_string))
-            self.output += case_string.strip(" ")
+        name, (models, cases) = method
+        print(cases)
+        assert len(models) == len(cases)
+        self.output += f"\n    def test_{name}(self):"
+        for i in range(len(models)):
+            for case in cases[i]:
+                case_string = f"\n        {models[i]}"
+                for x in range(len(case)):
+                    case_string = case_string.replace(f"X{x + 1}", case[x])
+                self.output += case_string.strip(" ")
 
-    def get_model(docstring):
-        m = re.findall("(?<=model:)[\W\D\S]*(?=cases:)", docstring)
+    def get_models(docstring):
+        m = re.findall("(?<=model:)[\S\W\D]*?(?=cases:)", docstring, re.MULTILINE)
         if m is None:
             raise ValueError(f"A model must be present in every docstring. Missing in docstring of function {function[1]}.")
             sys.exit()
-        return m[0]
-
-    def get_cases(docstring):
-        m = re.findall("(?<=model:)[\W\D\S]*(?=cases:)", docstring)
-        if m is None:
-            raise ValueError(f"A model must be present in every docstring. Missing in docstring of function {function[1]}.")
-        return m
-
-    # def filter_tests(classes):
-        # for i in range(len(classes)):
-            # functions = classes[i][1]
-            # for x in range(len(functions)):
-                # print(functions[x][1])
-                # m = re.search("\/\*\n*\t*tests[\W\D\S]*\*\/", functions[x][1])
-                # if m is None:
-                    # classes[i][1][x].pop(x)
-            # if len(classes[i][1]) == 0:
-                # classes.pop(i)
-        # return classes
+        return [i for i in m if len(i) > 0]
 
     def get_docstrings(self):
         parsed = ast.parse(self.file)
@@ -120,6 +127,7 @@ if __name__ == "__main__":
             file = open(sys.argv[1]).read()
         except IndexError:
             print("Make sure to call the file with ./ and dont' forget to add the file")
+            sys.exit()
 
     except FileNotFoundError:
         print("The file was not found")
